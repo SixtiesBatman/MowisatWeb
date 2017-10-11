7b1e1d75e3f0d452b7d00c5428a461ce837004d9
J0cker <manlioelnum1@hotmail.com>
2017-10-11 16:19:18 -0500

    Estandarización de código

--- index.php
+++ index.php
@@ -5,28 +5,10 @@
 <html>
     <head>
         <title>Mowisat Corporation</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />        
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="styles/style.css" rel="stylesheet" type="text/css">
-        <link rel="shortcut icon" href="images/favicon_mowisat.png">
-        <!-- Latest compiled and minified CSS -->
-        <link rel="stylesheet" href="styles/bootstrap.css">
 
-        <!-- jQuery library -->
-        <script src="js/jquery.js"></script>
-
-        <!-- Latest compiled JavaScript -->
-        <script src="js/bootstrap.js"></script>
-
-
-        <script src="js/toastr.js"></script>
-        <link rel="stylesheet" href="styles/toastr.css">
-
-        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-        
-        <link rel="stylesheet" href="styles/font-awesome.css">
-        <link rel="stylesheet" href="styles/fonts-apply.css">
-        
+        <?PHP
+            include ''.dirname(__FILE__).'/template/meta.php';
+        ?>
 
         <style>
             a {color: gray}
@@ -120,77 +102,11 @@
         </script>
 
     </head>
-    <body>
-        <header class="container-fluid">
-        <!-- FIRST PART OUT OF 3 OF THE HEADER -->    
-            <nav class="row dots" style="background-image: url('images/blue-dots.jpg')">
-                
-                <div class="col-sm-1"></div>
-                
-                <div class="col-sm-10">
-                    
-                    <div class="login-btn">
-                        <button>Login</button>
-                    </div>
-                        
-                </div>
-                
-                <div class="col-sm-1"></div>
-                
-            </nav>
-        
-        <!-- SECOND PART OUT OF 3 OF THE HEADER -->
-            <div class="row">
-                
-                <nav style="margin-bottom: 0 !important; border: 0 !important; background-color: #FFF !important; border-radius: 0 !important;" class="navbar navbar-inverse">
-                    <div class="container-fluid">
-                      <div class="navbar-header">
-                        <button style="background-color: black;" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>                        
-                        </button>
-                        <a class="header-two" href="#">
-                            <img src="images/mowisat-corp-logo.png" alt="mowisat corp logo" />        
-                        </a>
-                      </div>
-                      <div style="border: 0 !important;" class="collapse navbar-collapse" id="myNavbar">
-                        <ul style="margin: 1.2em 0 0 0 !important;" class="nav navbar-nav navbar-right nav-style-header">
-                            <li><a style="color: black;" href="#">satellite broadband</a></li>
-                            <li><a style="color: black;" href="#">social responsability</a></li>
-                            <li><a style="color: black;" href="#">news</a></li>
-                            <li><a style="color: black;" href="#">investors</a></li>
-                            <li class="hidden-xs hidden-sm round"><a style="color: black;" href="#">EN</a></li>
-                            <li class="hidden-xs hidden-sm round"><a style="color: black;" href="#">ES</a></li></ul>
-                      </div>
-                    </div>
-                  </nav>
-                
-            </div>    
-        
-        <!-- THIRD PART OUT OF 3 OF THE HEADER -->  
-            
-            <nav class="row header-three-style">
-                <div id="third-header">
+    <body>   
 
-                    <div class="col-xs-12 col-sm-4 col-md-4 style-box">
-                        <div class="inline-display"><img src="images/icon7.png" alt="question mark" />why mowisat?</div>
-                    </div>
-
-                    <div class="col-xs-12 col-sm-4 col-md-4 style-box">
-                        <div class="inline-display"><img src="images/icon8.png" alt="gears" />how it works?</div>
-                    </div>
-
-                    <div class="col-xs-12 col-sm-4 col-md-4 style-box">
-                        <div class="inline-display"><img src="images/icon9.png" alt="partners" />our partners</div>
-                    </div>
-
-                </div>
-            </nav>
-            
-        </header>
-    <!-- ENDS HEADER -->    
-
+    <?PHP
+        include ''.dirname(__FILE__).'/template/header.php';
+    ?>
 
     <!-- INITIATES SLIDER -->        
     <section class="jumbotron" style="margin-bottom: 0; background-image: url('images/tablet3.jpg')">
@@ -389,81 +305,7 @@ and sociocultural initiatives such as marginalized communities.</p>
         <!-- ENDS WORK WITH US BOX -->
     
     <!-- CONTAINERS SECTION ENDS -->
-    
 
-
-
-    <!-- INITIATES FOOTER -->
-    
-    <footer class="footer">
-        
-        <div class="row">
-            <div class="col-sm-12 social-network">
-                <ul>
-                    <li><div class="logos-sm1"><a href="#"><img src="images/twitter-logo.png" alt="twitter" />@MowisatCorp</a></div></li>
-                    <li><div class="logos-sm"><a href="#"><img src="images/fb-logo.png" alt="facebook" />Facebook.com/MowisatCorporation</a></div></li>
-                    <li><div class="logos-sm2"><a href="#"><img src="images/linkedin-logo.png" alt="linkedin" />Linkedin</a></div></li>
-                </ul>    
-            </div>
-        </div>
-        
-        <div class="row">
-            
-            <div class="col-sm-1"></div>
-            
-            <div class="col-sm-2 other-list">
-                <ul>
-                    <li class="blue-letter">Meet Mowisat</li>
-                    <li>Mision</li>
-                    <li>Vision</li>
-                    <li>Our team</li>
-                    <li>Section partners</li>
-                </ul>
-            </div>
-            
-            <div class="col-sm-2">
-                <div class="blue-letter special">NEWS</div>
-                <div class="blue-letter special">CARRERS</div>
-                
-            </div>
-            
-            <div class="col-sm-3">
-                <div class="blue-letter">STAY IN TOUCH</div>
-                <div class="special2">info@mowisatcorp.com</div>
-                <div class="blue-letter">Canada</div>
-                <div class="last-paragraph">First Canadian Place<br />
-                    100 King Street West Suite 5700<br />
-                    Toronto, ON<br />
-                    M5X 1C7</div>
-            </div>
-            
-            <div class="col-sm-3">
-             
-                    
-                <div class="blue-letter mexico">Mexico</div>
-                <div class="last-paragraph">Mowisat Corporation<br />
-                     Mexico City, Mexico<br />
-                     Calle Bahía de las Palmas 1 Verónica Anzures<br />
-                     113000, Ciudad de México, CDMX</div>
-               
-            </div>
-            
-            <div class="col-sm-1"></div>
-            
-        </div>
-           
-        
-        
-        <div class="row">
-                <nav class="col-sm-12" id="other-links">
-                    <ul>
-                        <li><a href="#">2017 Mowisat Corporation</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Policies and Terms</a></li>
-                    </ul>
-                </nav>
-        </div>    
-    </footer>    
-    <!-- ENDS FOOTER -->
-    </body>
-</html>
+    <?PHP
+    include ''.dirname(__FILE__).'/template/footer.php';
+    ?>
---
+++ template/meta.php
@@ -0,0 +1,24 @@
+<?PHP
+?>
+
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />        
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="styles/style.css" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" href="images/favicon_mowisat.png">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="styles/bootstrap.css">
+
+<!-- jQuery library -->
+<script src="js/jquery.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="js/bootstrap.js"></script>
+
+
+<script src="js/toastr.js"></script>
+<link rel="stylesheet" href="styles/toastr.css">
+
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+<link rel="stylesheet" href="styles/font-awesome.css">
+<link rel="stylesheet" href="styles/fonts-apply.css">
