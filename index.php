7f961b9b9e56a506e4f83ee949493546199cba8f
J0cker <manlioelnum1@hotmail.com>
2017-10-20 11:52:22 -0500

    Version final de juntar ricardo con lo mío

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-    include ''.dirname(__FILE__).'/scripts/utils/idiom.php';
-    include ''.dirname(__FILE__).'/scripts/utils/log.php';
-    include ''.dirname(__FILE__).'/scripts/utils/conexionBlog.php';
+
+include ''.dirname(__FILE__).'/scripts/utils/idiom.php';
+include ''.dirname(__FILE__).'/scripts/utils/log.php';
+include ''.dirname(__FILE__).'/scripts/utils/conexionBlog.php';
+
     $log = new LoggerPhp();
     $log->write_log("[Index]","Debug");
 ?>
@@ -61,35 +63,208 @@
     
         <!-- INITIATES INFO-BAR -->
     
-        <section class="container-fluid info-bar">
+    
+        
+        <!-- FIRST INFO-BAR LINE -->
+        <section class='container-fluid info-bar'>
+        
+            <div class='row'>
+                <div class='col-xs-12 col-md-12'>
+                    <h3 style="text-align: center; font-size: 4em; margin: 1em 0;">Get in touch with Mowisat's projects</h3>
+                </div>
+            </div>
+    
+        </section>
+    
+    
+        <!-- ENDS INFO-BAR -->
+    
+
+        <section class="container-fluid">
+
+            <div class="row" style="background-color: #569EB6; color: white; margin-top: 0;  text-align: center;">                
+                <div class="hidden-sm-1 col-md-1"></div>
+
+                <div class="col-sm-12 col-md-3 big-box logos">
+                
+                        <div class="transform" style="text-align: center;">
+                            <img src="images/dish3.jpg" />
+                        </div>
+        
+                </div>                
+        
+                <div class="col-sm-12 col-md-7 big-box">
+    
+                    <div class="pad">
+                        <h3 style="font-size: 4em; margin: 1em;  text-align: center;"><?PHP echo $txt["sat1"]; ?></h3>
+                        <p style="text-aling: center; padding: 0 2em;"><?PHP echo $txt["sat2"]; ?></p>
+                        <a href="https://pig.gi/">
+                            <button style="color: white; background-color: transparent; border: 4px solid white; border-radius: 2em; font-size: 1em; text-align: center; text-decoration: none; display: inline-block; padding: 0.35em 1.5em; margin: 2em;"><?PHP echo $txt["sat3"]; ?></button>
+                        </a>
+                    </div> 
+    
+                </div>
+
+                <div class="hidden-sm-1 col-md-1"></div>
+
+                </div>        
+               
+                
+                
+                
+            </div>
+
+        </section>
+
+        <!--- AQUIE MERO -->
+
+        <section class="container-fluid">
+
+            <div class="row" style="background-color: white; color: black; margin-top: 0;  text-align: center;">                
+                <div class="hidden-sm-1 col-md-1"></div>
+
+                    <div class="col-sm-12 col-md-7 big-box">
+        
+                        <div class="pad">
+                            <h3 style="font-size: 4em; margin: 1em;  text-align: center;"><?PHP echo $txt["soc1"]; ?></h3>
+                            <p style="text-aling: center; padding: 0 2em;"><?PHP echo $txt["soc2"]; ?></p>
+                            <a href="https://pig.gi/">
+                                <button style="color: black; background-color: transparent; border: 4px solid black; border-radius: 2em; font-size: 1em; text-align: center; text-decoration: none; display: inline-block; padding: 0.35em 1.5em; margin: 2em;"><?PHP echo $txt["soc3"]; ?></button>
+                            </a>
+                        </div> 
+        
+                    </div>
             
-            <!-- FIRST INFO-BAR LINE -->
-            <div class="row">
+                    <div class="col-sm-12 col-md-3 big-box logos">
+                    
+                            <div class="transform" style="text-align: center;">
+                                <img src="images/dish3.jpg" />
+                            </div>
+            
+                    </div>                
+    
+                <div class="hidden-sm-1 col-md-1"></div>
+            
                 
-                <div class="col-sm-2"></div>
-                
-                <div class="col-sm-4 inline-box">
-                    <div><a href="how-works.php"><img src="images/icon1.png" alt="schedular" /><?PHP echo $txt["schedular"]; ?></a></div>
-                    <div><a href="mission.php"><img src="images/icon2.png" alt="IoT" /><?PHP echo $txt["iot"]; ?></a></div>
-                </div>
-                <div class="col-sm-4 inline-box">
-                    <div><a href="satellite.php"><img src="images/icon4.png" alt="Wi-Fi" /><?PHP echo $txt["wifi"]; ?></div>
-                    <div><a href="social.php"><img src="images/icon5.png" alt="education" /><?PHP echo $txt["onlineEdu"]; ?></a></div>                
-                </div>
-                
-                <div class="col-sm-2"></div>
                 
             </div>
                     
           
         </section>
     
+        <section class="container-fluid">
+        
+            <div class="row" style="background-color: #569EB6; color: white; margin-top: 0;  text-align: center;">                
+                <div class="hidden-sm-1 col-md-1"></div>
+
+                <div class="col-sm-12 col-md-3 big-box logos">
+                
+                        <div class="transform" style="text-align: center;">
+                            <img src="images/dish3.jpg" />
+                        </div>
+        
+                </div>                
+        
+                <div class="col-sm-12 col-md-7 big-box">
     
-        <!-- ENDS INFO-BAR -->
+                    <div class="pad">
+                        <h3 style="font-size: 4em; margin: 1em;  text-align: center;"><?PHP echo $txt["mow1"]; ?></h3>
+                        <p style="text-aling: center; padding: 0 2em;"><?PHP echo $txt["mow2"]; ?></p>
+                        <a href="https://pig.gi/">
+                            <button style="color: white; background-color: transparent; border: 4px solid white; border-radius: 2em; font-size: 1em; text-align: center; text-decoration: none; display: inline-block; padding: 0.35em 1.5em; margin: 2em;"><?PHP echo $txt["mow3"]; ?></button>
+                        </a>
+                    </div> 
     
-    
+                </div>
+
+                <div class="hidden-sm-1 col-md-1"></div>
+
+                </div>        
+                
+                
+                
+                
+            </div>
+
+        </section>
+
     
         
+        <!-- INITIATES BLOG BOX -->
+        
+        <!-- FIRST BLOG BOX -->
+        <section class="container-fluid blog-box">
+        <div class="row">
+            
+            <div class="col-sm-1"></div>
+            
+            <div class="col-sm-5 blog-new">
+                <h4 class="margin">BLOG</h4>
+
+                <?php
+
+                base_de_datos_utf_8($conn);
+                $query = $conn->query("select post_content, post_title, guid from wp_posts WHERE post_type='post' ORDER BY ID DESC LIMIT 5") OR DIE(mysqli_error($conn));
+                if($query->num_rows>0){
+                    while($row=$query->fetch_assoc()){
+                        $title = $row["post_title"];
+                        ?>
+                        
+                        <h5 id="titleBlog">
+                            <a style="color: #85A0CF;" href="<?PHP echo $row["guid"]; ?>"><?PHP echo $title; ?></a>
+                        </h5>
+
+                        <?PHP
+                    }
+                }
+
+                ?>
+                
+                <p class="blog-p padding-right" id="contentBlog"></p>
+                <a style="display: none;" href="" id="readBlog">
+                    <button>Read more</button>
+                </a>
+            </div>
+            
+            
+            
+    <!-- SECOND BLOG BOX -->        
+            <div style="margin-bottom: 20px;" class="col-sm-5 blog-second">
+                <p class="blog-p margin padding-left"><?PHP echo $txt["blogPost"]; ?></p>
+                <div>
+                
+                <div class="row">
+                    <img style="margin-left: 100px;" src="images/name-location.png" />
+                </div>
+
+                <div class="row">
+
+                    <div class="col-xs-12 col-sm-12 col-md-12">
+
+                        <div class='image col-xs-6 col-sm-6 col-md-6'>
+                            <img style="text-align: center; border-radius: 100%; height: 10em; width: 10em;" src="images/people/josefina.jpg" />
+                        </div>
+
+
+                        <div class='image col-xs-6 col-sm-6 col-md-6 text-center'>
+                            <b>Josefina<br />
+                            Tizilingo, CDMX</b>
+                        </div>
+
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    
+            <div class="col-sm-1"></div>
+             
+        </div>
+    </section>
+    
+    <!-- ENDS BLOG BOX -->
+    
+    
         <!-- INITIATES CONTACT US BOX -->
         
         <section class="container contact-us-box">
@@ -156,82 +331,9 @@
         
     
         <!-- ENDS CONTACT US BOX -->
-        
-        <!-- INITIATES BLOG BOX -->
-        
-        <!-- FIRST BLOG BOX -->
-        <section class="container-fluid blog-box">
-            <div class="row">
-                
-                <div class="col-sm-1"></div>
-                
-                <div class="col-sm-5 blog-new">
-                    <h4 class="margin">BLOG</h4>
-
-                    <?php
-
-                    base_de_datos_utf_8($conn);
-                    $query = $conn->query("select post_content, post_title, guid from wp_posts WHERE post_type='post' ORDER BY ID DESC LIMIT 5") OR DIE(mysqli_error($conn));
-                    if($query->num_rows>0){
-                        while($row=$query->fetch_assoc()){
-                            $title = $row["post_title"];
-                            ?>
-                            
-                            <h5 id="titleBlog">
-                                <a style="color: #85A0CF;" href="<?PHP echo $row["guid"]; ?>"><?PHP echo $title; ?></a>
-                            </h5>
-
-                            <?PHP
-                        }
-                    }
-
-                    ?>
-                    
-                    <p class="blog-p padding-right" id="contentBlog"></p>
-                    <a style="display: none;" href="" id="readBlog">
-                        <button>Read more</button>
-                    </a>
-                </div>
-                
-                
-                
-        <!-- SECOND BLOG BOX -->        
-                <div style="margin-bottom: 20px;" class="col-sm-5 blog-second">
-                    <p class="blog-p margin padding-left"><?PHP echo $txt["blogPost"]; ?></p>
-                    <div>
-                    
-                    <div class="row">
-                        <img style="margin-left: 100px;" src="images/name-location.png" />
-                    </div>
-
-                    <div class="row">
-
-                        <div class="col-xs-12 col-sm-12 col-md-12">
-
-                            <div class='image col-xs-6 col-sm-6 col-md-6'>
-                                <img style="text-align: center; border-radius: 100%; height: 10em; width: 10em;" src="images/people/josefina.jpg" />
-                            </div>
 
 
-                            <div class='image col-xs-6 col-sm-6 col-md-6 text-center'>
-                                <b>Josefina<br />
-                                Tizilingo, CDMX</b>
-                            </div>
-
-                        </div>
-                    </div>
-                    
-                </div>
-            </div>
-        
-                <div class="col-sm-1"></div>
-                 
-            </div>
-        </section>
-        
-        <!-- ENDS BLOG BOX -->
-        
-        
+    
         <!-- INITIATES WORK WITH US BOX -->
         
         <section class="container-fluid work-with-us-box">
